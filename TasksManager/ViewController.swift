e0bf752e58085eaf88a0faa1e980288f72aafc4e
Artem Syritsa <80500451040j@gmail.com>
2020-02-03 23:14:27 +0200

    Initial Commit

---
+++ TasksManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TasksManager
+//
+//  Created by Artem Syritsa on 03.02.2020.
+//  Copyright © 2020 Artem Syritsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
